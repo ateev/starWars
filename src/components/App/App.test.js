c12ded750a5273c5e7c5cbe89da7491b7fa9fb5a
ateev <ateev@live.com>
2017-03-19 18:15:14 +0530

    unit tests updated

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import App from './App.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const component = shallow(
+        <App />
+    );
+    expect(component).toMatchSnapshot();
+  });
+})
