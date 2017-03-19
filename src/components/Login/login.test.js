1385a89d03fac5bb4a2115ab75d9422440c37f0b
ateev <ateev@live.com>
2017-03-19 20:16:43 +0530

    added correct file name

--- src/components/Login/login.test.js
+++ src/components/Login/login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Login from './Login.js';
+import Login from './login.js';
 
 describe('<Login />', () => {
     it('renders without crashing', () => {
